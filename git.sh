0e704e00a32e95f261fffbee8c0e3c91197cdf6b
Arjun Thakur <thakurarjun247@gmail.com>
2025-12-02 13:08:12 +0530

    added git script

---
+++ git.sh
@@ -0,0 +1,27 @@
+#Now, you can run the script like this:
+# sh git.sh "Your commit message here"
+
+#This line specifies that the script should be executed using the Bash shell. It's essential for running Bash scripts.
+#!/bin/bash
+
+
+git pull
+
+git add .
+
+git status
+
+# Check if a commit message is provided as a command-line argument
+if [ -n "$1" ]; then
+  commitMessage="$*"
+else
+  echo "\nEnter a commit message:"
+  read commitMessage
+fi
+
+git commit -m "$commitMessage"
+
+
+git push
+
+echo "Pushed to Prod!\n"
